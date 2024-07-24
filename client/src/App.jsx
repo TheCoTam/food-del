c6e2ca91bf4831f869360fbb2ef2297acfce4418
TheCoTam <hoangnguyenanhtuyen@gmail.com>
2024-07-24 11:55:35 +0700

    init project

---
+++ client/src/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "@/components/Navbar/navbar";
+
+const App = () => {
+  return (
+    <div className="w-[80%] m-auto">
+      <Navbar />
+    </div>
+  );
+};
+
+export default App;
