efcd2802d0e18fb6c753a79f199c0e1968c35622
TheCoTam <hoangnguyenanhtuyen@gmail.com>
2024-07-24 16:39:19 +0700

    thêm định tuyến cho các trang

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,22 @@
-import Navbar from "@/components/Navbar/navbar";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import Navbar from "@/components/navbar";
+import Home from "@/pages/Home/home";
+import Cart from "@/pages/Cart/cart";
+import PlaceOrder from "@/pages/PlaceOrder/place-order";
 
 const App = () => {
   return (
-    <div className="w-[80%] m-auto">
-      <Navbar />
-    </div>
+    <BrowserRouter>
+      <div className="w-[80%] m-auto">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cart" element={<Cart />} />
+          <Route path="/order" element={<PlaceOrder />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
