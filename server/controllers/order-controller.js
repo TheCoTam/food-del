f67010c617a95f27c2c480d2454b40221d6e5752
TheCoTam <hoangnguyenanhtuyen@gmail.com>
2024-07-27 19:28:06 +0700

    tạo api lấy các đơn hàng đã đặt

--- server/controllers/order-controller.js
+++ server/controllers/order-controller.js
@@ -3,6 +3,7 @@ import userModel from "../models/user-model.js";
 
 // placing user's order
 export const placeOrder = async (req, res) => {
+  const clientUrl = process.env.CLIENT_URL;
   try {
     const { userId, items, amount, address } = req.body;
 
@@ -16,9 +17,48 @@ export const placeOrder = async (req, res) => {
     await newOrder.save();
     await userModel.findByIdAndUpdate(userId, { cartData: {} });
 
-    res.json({ success: true, message: "Order Placed" });
+    const successUrl = `${clientUrl}/verify?success=true&orderId=${newOrder._id}`;
+    const errorUrl = `${clientUrl}/verify?success=false&orderId=${newOrder._id}`;
+
+    res.json({ success: true, message: "Order Placed", successUrl, errorUrl });
   } catch (error) {
     console.log(error);
+    res.json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+// verifying user's order
+export const verifyOrder = async (req, res) => {
+  const { orderId, success } = req.body;
+  console.log(orderId, success);
+  try {
+    const order = await orderModel.findById(orderId);
+
+    if (!order) {
+      return res.json({ success: false, message: "Order Not Found" });
+    }
+
+    if (success === "true") {
+      await orderModel.findByIdAndUpdate(orderId, { payment: true });
+      res.json({ success: true, message: "Order Paid" });
+    } else {
+      await orderModel.findByIdAndDelete(orderId);
+      res.json({ success: false, message: "Order Failed" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Internal Server Error" });
+  }
+};
+
+// user orders
+export const userOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({ userId: req.body.userId });
+
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log("[Order controller]", error);
     res.json({ success: false, message: "Internal Server Error" });
   }
 };
--- server/routes/order-route.js
+++ server/routes/order-route.js
@@ -1,10 +1,16 @@
 import express from "express";
 
 import authMiddleware from "../middleware/auth.js";
-import { placeOrder } from "../controllers/order-controller.js";
+import {
+  placeOrder,
+  userOrders,
+  verifyOrder,
+} from "../controllers/order-controller.js";
 
 const orderRouter = express.Router();
 
 orderRouter.post("/place", authMiddleware, placeOrder);
+orderRouter.post("/verify", verifyOrder);
+orderRouter.post("/userOrders", authMiddleware, userOrders);
 
 export default orderRouter;
